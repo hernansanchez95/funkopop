3a71efa44476a2ea60e522a335dacc34cd7e1d3f
Hernan <hernansanchez395@gmail.com>
2020-07-24 15:43:44 -0300

    se agrego el alta de funkopop

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,3 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import '../css/style.css'
+import Funko from './funko.js'
+
+//inicializo variables
+let listaFunkos = [];
+leerProductos();
+
+//function agregarFunko(event){
+window.agregarFunko = function (event){
+
+    event.preventDefault();
+
+    let codigo = document.getElementById('codigo').value;
+    let nombre = document.getElementById('nombre').value;
+    let numSerie = document.getElementById('numSerie').value;
+    let categoria = document.getElementById('categoria').value;
+    let descripcion = document.getElementById('descripcion').value;
+    let imagen = document.getElementById('imagen').value;
+    let precio = document.getElementById('precio').value;
+    //validar los datos del formulario
+
+    let nuevoFunko = new Funko(codigo,nombre, numSerie,categoria,descripcion,imagen,precio);
+    
+    console.log(nuevoFunko);
+
+    listaFunkos.push(nuevoFunko);
+    localStorage.setItem("funkoKey", JSON.stringify(listaFunkos));
+
+    limpiarFormulario();
+}
+
+function limpiarFormulario(){
+    let formulario = document.getElementById('formProducto');
+    formulario.reset();
+}
+
+function leerProductos(){
+    if(localStorage.length > 0){
+        
+        let _listaFunkos = JSON.parse(localStorage.getItem("funkoKey"));
+        if(listaFunkos.length == 0){
+            listaFunkos = _listaFunkos;
+        }
+    }
+}
--- src/js/funko.js
+++ src/js/funko.js
@@ -1,10 +1,12 @@
 export default class Funko{
-    constructor(codigo, nombre, numSerie, descripcion, imagen, precio){
+    constructor(codigo, nombre, numSerie, categoria, descripcion, imagen, precio){
         this.codigo = codigo;
         this.nombre = nombre;
         this.numSerie = numSerie;
+        this.categoria = categoria;
         this.descripcion = descripcion;
         this.imagen = imagen;
         this.precio = precio;
     }
 }
+
